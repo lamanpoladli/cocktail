9ec9e2ca95228b78b7d754439c85fed5d8909d72
lamanpoladli <lamanpoladli@gmail.com>
2023-06-22 00:01:24 +0400

    back problems

--- frontend/src/pages/Main/Menu/index.jsx
+++ frontend/src/pages/Main/Menu/index.jsx
@@ -1,22 +1,42 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./_index.scss";
-import { deleteCategoryByID, getAllCategories } from "../../../api/requests";
+import { deleteCategoryByID, getAllCategories, getCategoryByID, getCategoryProducts, postProduct } from "../../../api/requests";
 import { useCategoryContext } from "../../../context/categoryContext";
+import { useFormik } from "formik";
 
 const Index = () => {
   const[categories,setCategories] = useState();
+  const [category, setCategory] = useState({});
+  const [products, setProducts] = useState([]);
   const[loading,setLoading] = useState(true);
+  const { id } = useParams();
   useEffect(() => {
     getAllCategories().then((res) => {
       setCategories(res);
       setLoading(false);
     });
-  },[]);
-  function handleSearch(e) {
-    getAllCategories(e.target.value).then((res) => {
-      setCategories(res);
+  },[]); 
+  useEffect(() => {
+    getCategoryProducts(id).then((res) => {
+      setProducts(res);
     });
+  }, [id]);
+  async function handleSubmit(values, actions) {
+    console.log("product data: ", values);
+    values.categoryID = id;
+    await postProduct(values);
+    setProducts([...products, values]);
+    actions.resetForm();
   }
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      title: "",
+      price: "",
+    },
+    onSubmit: handleSubmit,
+  });
   return (
     <>
       <div className="menu">
@@ -28,6 +48,13 @@ const Index = () => {
       <div key={category._id}>
         <h1>{category.name}</h1>
         <p>{category.description}</p>
+        {products.map((product) => (
+        <div key={product._id}>
+          <p>{product.name}</p>
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+        </div>
+        ))}
       </div>
             )})}
 
